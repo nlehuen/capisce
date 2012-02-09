0817a26da93b8d584b525cda0e1556ed7d617686
Nicolas Lehuen <nicolas@lehuen.com>
2012-02-09 14:47:51 +0100

    Initial commit

---
+++ test/test2.js
@@ -0,0 +1,42 @@
+var capisce = require('../lib/capisce.js');
+
+function test2() {
+	var res1, res2, res3=[];
+
+	capisce.sequence().perform(function(over) {
+		console.log("Starting res1");
+		setTimeout(function() {
+			console.log("Ended res1");
+			res1 = "Hello";
+			over();
+		}, 1000);
+	}).then(function(over) {
+		console.log("Starting res2");
+		setTimeout(function() {
+			console.log("Ended res2");
+			res2 = "World";
+			over();
+		}, 1000);
+	}).thenConcurrently(function(parallel, over) {
+		var i;
+		var count = 0;
+		for(i=0;i<10;i++) {
+			parallel.perform(function(over) {
+				var c = count++;
+				console.log("Starting final job "+c);
+				setTimeout(function() {
+					var text = res1+", "+res2+":"+c;
+					res3.push(text)
+					console.log(text);
+					over();
+				}, Math.random()*2000);
+			});
+		}
+		parallel.whenDone(over);
+	}, 4).then(function() {
+     	console.log("res3.length="+res3.length);
+		console.log("All done !");
+	});
+}
+
+test2();
